69b04b99eb5b13f6a6d87731a97b32c420fd0c26
AkhilRokkam <akhilrokkam111@gmail.com>
2020-04-21 12:15:16 +0530

    Customer

---
+++ Customer.java
@@ -0,0 +1,120 @@
+package customer_table_booking;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class Customer {
+
+	private Long id;
+	private String name;
+	private String mobileNumber;
+	private Date birthdate;
+	private Double averageSpendAmount;
+	private Double totalAmount;
+	private Date dateEnrolled;
+	private Double rating;
+
+	public Customer() {
+
+	}
+
+	public Customer(Long id, String name, String mobileNumber, Date birthdate, Double averageSpendAmount,
+			Double totalAmount, Date dateEnrolled, Double rating) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.mobileNumber = mobileNumber;
+		this.birthdate = birthdate;
+		this.averageSpendAmount = averageSpendAmount;
+		this.totalAmount = totalAmount;
+		this.dateEnrolled = dateEnrolled;
+		this.rating = rating;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getMobileNumber() {
+		return mobileNumber;
+	}
+
+	public void setMobileNumber(String mobileNumber) {
+		this.mobileNumber = mobileNumber;
+	}
+
+	public Date getBirthdate() {
+		return birthdate;
+	}
+
+	public void setBirthdate(Date birthdate) {
+		this.birthdate = birthdate;
+	}
+
+	public Double getAverageSpendAmount() {
+		return averageSpendAmount;
+	}
+
+	public void setAverageSpendAmount(Double averageSpendAmount) {
+		this.averageSpendAmount = averageSpendAmount;
+	}
+
+	public Double getTotalAmount() {
+		return totalAmount;
+	}
+
+	public void setTotalAmount(Double totalAmount) {
+		this.totalAmount = totalAmount;
+	}
+
+	public Date getDateEnrolled() {
+		return dateEnrolled;
+	}
+
+	public void setDateEnrolled(Date dateEnrolled) {
+		this.dateEnrolled = dateEnrolled;
+	}
+
+	public Double getRating() {
+		return rating;
+	}
+
+	public void setRating(Double rating) {
+		this.rating = rating;
+	}
+
+	public String toString() {
+		return "Id:" + id + "\nName:" + this.name + "\nMobile Number:" + this.mobileNumber + "\nDate of Birth:"
+				+ this.birthdate + "\nAverage spent amount:" + this.averageSpendAmount + "\nTotal amount:"
+				+ this.totalAmount + "\nDate Enrolled:" + this.dateEnrolled + "\nRating:" + this.rating;
+	}
+
+	public boolean equals(Object obj) {
+		if (obj instanceof Customer) {
+			Customer cus = (Customer) obj;
+
+			if (!this.mobileNumber.equals(cus.mobileNumber)) {
+				return false;
+			}
+			SimpleDateFormat sdf = new SimpleDateFormat("dd-MM-yyyy");
+			String d1 = sdf.format(this.birthdate);
+			String d2 = sdf.format(cus.birthdate);
+			if (!d1.equals(d2)) {
+				return false;
+			}
+		}
+		return true;
+	}
+}
